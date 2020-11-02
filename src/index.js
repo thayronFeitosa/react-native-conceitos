b64d75e278b5e21dc18340279279d63adc62cf6e
thayron <thayronf3@gmail.com>
2020-11-02 19:37:25 -0300

    Principais conceitos do react-native e as diferenças entre react-native e reactjs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import React from 'react';
-import {View} from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+//Não possuem valor semâmtico (significado)7
+//Não possuem estilização propria
+// todos componentes possuem por padrão "displey: flex";
 
-export default function App(){
-    return <View />
+
+//view: div footer, header, main, aside, section
+//Text: p, spam, strong, h1, h2, h3
+
+export default function App() {
+    return (
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+            <View style={styles.container} >
+                <Text style={styles.title} >Hello word</Text>
+            </View>
+        </>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7159c1',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        color: '#FFF',
+        fontSize: 32,
+        fontWeight: 'bold'
+    }
+});
